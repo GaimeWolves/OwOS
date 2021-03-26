fbf8905811a014c7170042a7c3cbe67e4989d05f
GaimeWolves <gamewolvesofficial@gmail.com>
2021-03-26 21:39:54 +0100

    Added rudimentary support for C++ global constructors
    CMake will first build the crtx.o object files as seperate objects
    files. Then, in the kernel target, it will run a pre-link command to
    copy the generated object files to a known location. (I couldn't find a
    different way to access $<TARGET_OBJECTS:...>)
    Now the CMAKE_CXX_LINK_EXECUTABLE is setup to link the crtx.o files in
    the needed order. entry.cpp now contains a small test function for
    global constructors.
    
    TODO: Find a better way to specify the crtx.o files order in CMake
    TODO: Clean up entry messages with a rudimentary tty or serial interface

--- src/kernel/entry.cpp
+++ src/kernel/entry.cpp
@@ -8,6 +8,14 @@ static inline uint16_t vga_entry(uint8_t uc, uint8_t color)
     return (uint16_t)uc | (uint16_t)color << 8;
 }
 
+__attribute__((constructor)) void testCRTX()
+{
+    char message[] = "HELLO FROM CRTX!";
+
+        for (size_t i = 0; i < sizeof(message) - 1; i++)
+        *(((uint16_t *)0xB8000) + i + 80) = vga_entry((uint8_t)message[i], 15);
+}
+
 extern "C" __attribute__((noreturn)) void entry(uint32_t magic, multiboot_info_t *multiboot_info)
 {
     char message[] = "HELLO WORLD!";
@@ -16,17 +24,17 @@ extern "C" __attribute__((noreturn)) void entry(uint32_t magic, multiboot_info_t
 
     if (magic != MULTIBOOT_BOOTLOADER_MAGIC)
     {
-        for (size_t i = 0; i < 11; i++)
+        for (size_t i = 0; i < sizeof(wrong_magic_error) - 1; i++)
             *(((uint16_t *)0xB8000) + i) = vga_entry((uint8_t)wrong_magic_error[i], 15);
     }
 
     if (!(multiboot_info->flags & MULTIBOOT_INFO_MEM_MAP))
     {
-        for (size_t i = 0; i < 10; i++)
+        for (size_t i = 0; i < sizeof(no_mem_error) - 1; i++)
             *(((uint16_t *)0xB8000) + i) = vga_entry((uint8_t)no_mem_error[i], 15);
     }
 
-    for (size_t i = 0; i < 12; i++)
+    for (size_t i = 0; i < sizeof(message) - 1; i++)
         *(((uint16_t *)0xB8000) + i) = vga_entry((uint8_t)message[i], 15);
 
     for (;;)
